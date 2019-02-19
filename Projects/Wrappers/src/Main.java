8731df443a9e60be61bbceca970a1b014ee4ec5f
Anton <ant.fedorov@mail.ru>
2019-02-19 20:43:41 +0300

    add task 26.

---
+++ Projects/Wrappers/src/Main.java
@@ -0,0 +1,12 @@
+/*
+Классы обертки
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer x = null;
+        int a = 1;
+        x = a;
+        char b = 10042;
+        System.out.println(b);
+    }
+}
